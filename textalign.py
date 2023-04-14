b5cf90cf82bd820e599f85a8974d82c806fa1cff
barioder <barioder27@gmail.com>
2023-04-14 18:05:54 +0300

    align text right

---
+++ textalign.py
@@ -0,0 +1,9 @@
+width = 20
+print(width)
+values = 80
+# right aligned 
+print("This text is aligned right".rjust(values, '-'))
+
+#  left aligned 
+
+print("This text is aligned right".ljust(values, '-'))
