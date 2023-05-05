cb74b59dd1c5bd9bcde2165564de9d7a009323c3
barioder <barioder27@gmail.com>
2023-05-05 22:06:01 +0300

    simple python class

---
+++ Classes/classes.py
@@ -0,0 +1,13 @@
+class Hen:
+    attribute1 = "Bird"
+    attribute2 = "It is a hen"
+
+
+# to create an object from the class 
+our_hen_object = Hen()
+
+# we can access the object values like  this 
+
+print(our_hen_object.attribute1)
+print(our_hen_object.attribute2)
+
