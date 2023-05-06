af116d7924e74147d5bc6d8d9145a268e5c0b248
barioder <barioder27@gmail.com>
2023-05-06 19:14:35 +0300

    Use of __init__ function in classes

--- Classes/classes.py
+++ Classes/classes.py
@@ -21,3 +21,34 @@ print(our_hen_object.attribute2)
 # calling the function from the class
 our_hen_object.actions()
 
+
+# Below is a class with the __inti__ function 
+# This allows the attribute to be defined at the time of creation 
+print("----------------look below for __init__ use output -------------------------")
+class Car:
+    def __init__(self, name,model, year,wheels):
+        self.name = name 
+        self.model = model
+        self.year = year 
+        self.wheels = wheels
+
+    
+    def neededCar(self):
+        print("The car name is", self.name)
+        print("The model is", self.model)
+        print("The year of make is", self.year)
+        print("This makes the car a ", self.wheels)
+
+
+# defining the car object 
+
+toyota = Car("TOYOTA", "ALTEZA", 2019, "4 Wheels")
+
+toyota.neededCar()
+
+print("For the benz object -----------")
+benz = Car("Benz", "4-Matic", "2018", "4 wheels")
+
+benz.neededCar()
+
+
