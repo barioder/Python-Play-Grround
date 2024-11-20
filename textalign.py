5fa3892b6a70b3ebbfe088f084fc588ac311717c
barioder <barioder27@gmail.com>
2024-11-20 11:58:58 +0300

    more on text align

--- textalign.py
+++ textalign.py
@@ -1,12 +1,15 @@
 
-values = 80
+width = 50
 # right aligned 
-print("This text is aligned right".rjust(values, '-'))
+print("This text is aligned right".rjust(width, '-'))
 
 #  center aligned  
 
-print("This text is Center aligned".center(values, '-'))
+print("This text is Center aligned".center(width, '-'))
 
 # left aligned 
 
-print("This text is left aligned".ljust(values, '-'))
+print("This text is left aligned".ljust(width, '-'))
+
+# it is optional to add the a fill value after the length of the width of the string
+print("On the left".rjust(width)+" joined here "+"On the right".ljust(width,"*"))
